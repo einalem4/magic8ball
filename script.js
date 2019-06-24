3988838ae970d6ddddf35f7a1c875313f9aca135
Melanie Arnold <43861102+einalem4@users.noreply.github.com>
2019-06-24 12:59:49 -0600

    removed immediate prompt
    prompt will show up when "ask me anything" button is clicked

--- script.js
+++ script.js
@@ -19,9 +19,14 @@ $(document).ready(function(){
       
     };
  
-    var onClick = function() {
-        var question = prompt("ASK A YES/NO QUESTION!");
-        magic8Ball.askQuestion(question);
+  var onClick = function() {
+       //wait half a second before showing prompt
+    setTimeout(
+        function() {
+            //show prompt
+            var question = prompt("Ask a yes or no question")
+            magic8Ball.askQuestion(question)
+        }, 500);
     };
  
     $("#questionButton").click( onClick );
@@ -29,13 +34,5 @@ $(document).ready(function(){
     $("#answer").hide();
  
     $("#8ball").attr("src", "https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/magic8ballQuestion.png");
- 
-    //wait half a second before showing prompt
-    setTimeout(
-       function() {
-           //show prompt
-           var question = prompt("Ask a yes or no question")
-           magic8Ball.askQuestion(question)
-       }, 500);
  
 });
